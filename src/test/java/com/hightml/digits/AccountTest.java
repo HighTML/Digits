594b7be06c7211668ef753f8f2a3b260ac6510f5
Marcel Heemskerk <marcel.heemskerk@gmail.com>
2017-11-30 00:39:07 +0100

    And added validation test case

--- src/test/java/com/hightml/digits/AccountTest.java
+++ src/test/java/com/hightml/digits/AccountTest.java
@@ -2,14 +2,21 @@ package com.hightml.digits;
 
 import com.hightml.digits.account.Account;
 import com.hightml.digits.account.AccountValidator;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import javax.validation.*;
+
+import java.util.Set;
+
 import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 /**
  * Created by Marcel Heemskerk (ra52ba) on 29-11-2017.
  */
+@Slf4j
 public class AccountTest {
 
     @Test
@@ -20,4 +27,39 @@ public class AccountTest {
         Account b = new Account(736160222);
         assertFalse(AccountValidator.isValidAccount(b));
     }
+
+
+    @Valid
+    Account goodAccount = new Account(736160221);
+
+
+
+    @Valid
+    Account badAccount = new Account(736160222);
+
+
+
+
+
+
+
+
+
+    @Test
+    public void testValidation() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+
+        Set<ConstraintViolation<Account>> violations = validator.validate(goodAccount);
+        assertTrue(violations.isEmpty());
+
+        violations = validator.validate(badAccount);
+        assertFalse(violations.isEmpty());
+        assertEquals(violations.iterator().next().getMessage(), "Account checksum is invalid");
+        log.debug("Violations {}", violations);
+
+    }
+
+
+
 }
